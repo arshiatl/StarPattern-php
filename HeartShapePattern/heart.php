9f2facbe5f46c970ca1ab57f41b58bd603ecd7f3
arshia kian <98839859+arshiatl@users.noreply.github.com>
2022-04-19 15:01:15 +0430

    Update heart.php

--- HeartShapePattern/heart.php
+++ HeartShapePattern/heart.php
@@ -2,7 +2,7 @@
 
 //heart shape pattern in <3 php <3
 
-echo "<h2 style='text-align: center;'>";
+echo "<h2 style='text-align: center; font-size:40px'>";
 for ($r = 0; $r < 6; $r++){
     for ($c = 0; $c < 7; $c++){
        if( ($r == 0 && $c % 3 != 0) || ($r == 1 && $c % 3 == 0) || ($r - $c == 2) || ($r + $c == 8)){
